6a423d476737c403a18b30a3779b4424a76ca04e
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-04-15 17:42:56 -0300

    Resolução da questão 3 da lista de PW2

---
+++ questao03/index.ts
@@ -0,0 +1,8 @@
+export const quest3 = (arr1: string[], arr2: string[]): boolean => {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    return arr1.every((element, index) => element === arr2[index]);
+  }
+}
